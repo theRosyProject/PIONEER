b04c236d77e1c8d8dc25c1eda500f71e0866fe40
Federico Dallo <36605177+theRosyProject@users.noreply.github.com>
2021-05-04 16:09:05 +0200

    Update goi2cBME280_py3.py

--- goi2cBME280_py3.py
+++ goi2cBME280_py3.py
@@ -26,10 +26,6 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 bme280.sea_level_pressure = 1013.25
 
 while True:
-    # print("\nTemperature: %0.1f C" % bme280.temperature)
-    # print("Humidity: %0.1f %%" % bme280.relative_humidity)
-    # print("Pressure: %0.1f hPa" % bme280.pressure)
-    # print("Altitude = %0.2f meters" % bme280.altitude)
     timestr = time.strftime("%Y-%m-%d %H:%M:%S")
     today = time.strftime("%Y-%m-%d")
     
