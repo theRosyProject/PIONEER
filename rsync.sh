0bb0d6444c0917220354e7f13cc9e90ee88dc117
Federico Dallo <36605177+theRosyProject@users.noreply.github.com>
2021-05-04 16:12:59 +0200

    Update and rename rsyncUther.sh to rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 #
-#	PROGRAMMA PER SINCRONIZZARE I DATI DI OZONO
-#	SU UTHER, SERVER OPENVPN A VENEZIA
+#	PROGRAMMA PER SINCRONIZZARE I DATI
+#	SUL SERVER OPENVPN
 #
 # BEGIN(VARIABILI)*da modificare (BASH non ha le variabili di ambiente!)
 USER=pi
-PROG_NAME=rsync_to_Uther
+PROG_NAME=rsync_to_openvpnServer
 # Le seguenti sono normalmente costanti
 DATE=`date +%Y-%m-%d`
 WORKDIR=/home/$USER/$PROG_NAME
 PIDFILE=/home/$USER/$PROG_NAME.pid
 #
-# dumb ##MRG_O3_i2c_addr_49_2019-10-16.txt
 FILE_48="REG_MAR_O3_i2c_addr_48_"$DATE".txt"
 FILE_49="REG_MAR_O3_i2c_addr_49_"$DATE".txt"
 FILE_BME="REG_MAR_BME280_i2c_"$DATE".txt"
 #FILE_22="MRG_T_RH_gpio22_sensor_"$DATE".txt"
 #FILE_23="MRG_T_RH_gpio23_sensor_"$DATE".txt"
-#
-#FILENAME=calibration_addr_48_$DATE.txt
-#FILENAME=
 # END(VARIABILI)
 #
 if [ -f $PIDFILE ]
@@ -51,8 +47,6 @@ else
     cp /home/pi/PIONEER/measurements/$FILE_48 /home/pi/PIONEER/measurements/toSync/
     cp /home/pi/PIONEER/measurements/$FILE_49 /home/pi/PIONEER/measurements/toSync/
     cp /home/pi/PIONEER/measurements/$FILE_BME /home/pi/PIONEER/measurements/toSync/
-    #cat /home/pi/o3net/measurements/localdata_addr_48.txt >> /home/pi/o3net/measurements/ozono_all_station1_addr_48.txt && > /home/pi/o3net/measurements/localdata_addr_48.txt
-    #rclone copy /home/pi/o3net/measurements/T_RH_all_station1.txt gdrive:OZO/
     rsync -avui /home/pi/PIONEER/measurements/toSync/ kopu@10.8.0.1:./OZO/toSync
     rm /home/pi/PIONEER/measurements/toSync/*
     echo "$PROG_NAME finished: `date`"
