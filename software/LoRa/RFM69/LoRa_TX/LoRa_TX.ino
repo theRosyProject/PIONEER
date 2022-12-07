ae0e420b7f5b6e6e144c821322f9918f6e3caff4
Federico Dallo <36605177+theRosyProject@users.noreply.github.com>
2022-12-06 16:21:56 -0800

    fix: now sending packet only if serial message is complete

--- software/LoRa/RFM69/LoRa_TX/LoRa_TX.ino
+++ software/LoRa/RFM69/LoRa_TX/LoRa_TX.ino
@@ -148,20 +148,26 @@ void setup()
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
 
+  // Write in console that system is ready
+  Serial.println("End Setup. Console ready");
 }
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void loop()
 {
-
   // send through LoRa a string when a newline arrives:
   if (stringComplete) {
+    Serial.println("Transmitting..."); // Send a message to rf95_server
     Serial.println(inputString);
 
-    // remove the new line character at the end of the string
-    //inputString.remove(inputString.length() - 1);
-    // trim the message
+    // Prepare the message to be sent
+    // Get datetime
+    DateTime now = rtc.now();
+    char buf1[20];
+    sprintf(buf1, "%02d%02d%02d%02d%02d%02d ",  now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second() );
+
+    // Remove the new line character at the end of the message string
     inputString.trim();
     // lenght of the string
     int str_len = inputString.length() + 1;
@@ -170,63 +176,86 @@ void loop()
     // copy the message over the array
     inputString.toCharArray(char_array, str_len);
 
+    // Prepare the character array for sending
+    int lora_msg_len = 20 + str_len;
+    char LoRa_message[lora_msg_len];
+
+    strcpy(LoRa_message, buf1);
+    strcat(LoRa_message, char_array);
+
     // send over LoRa
-    rf95.send((uint8_t *)char_array, str_len);
+    //rf95.send((uint8_t *)char_array, str_len);
+    rf95.send((uint8_t *)LoRa_message, lora_msg_len);
+
+    // Wait for message to get back
+    Serial.println("Waiting for packet to complete..."); 
+    delay(10);
+    rf95.waitPacketSent();
+    // Now wait for a reply
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+
+    Serial.println("Waiting for reply...");
+    if (rf95.waitAvailableTimeout(1000))
+    { 
+      // Should be a reply message for us now   
+      if (rf95.recv(buf, &len))
+      {
+        Serial.print("Got reply: ");
+        Serial.println((char*)buf);
+        Serial.print("RSSI: ");
+        Serial.println(rf95.lastRssi(), DEC);
+      }
+      else
+      {
+        Serial.println("Receive failed");
+      }
+    }
+    else
+    {
+      Serial.println("No reply, is there a listener around?");
+    }
 
     // clear the string:
     inputString = "";
     stringComplete = false;
   }
 
-  //delay(1000*60); // Wait 1 second between transmits, could also 'sleep' here!
-  Serial.println("Transmitting..."); // Send a message to rf95_server
-  
-  // char radiopacket[20] = "Hello World #      ";
-  // itoa(packetnum++, radiopacket+13, 10);
-  // Serial.print("Sending "); Serial.println(radiopacket);
-  // radiopacket[19] = 0;
-  
-  // Serial.println("Sending...");
+  // // Prepare a message to be sent
+  // DateTime now = rtc.now();
+  // char buf1[20];
+  // sprintf(buf1, "%02d%02d%02d%02d%02d%02d",  now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second() );
+  // //myString = buf1 + myString;
+  // //Serial.println(myString);
+  // rf95.send((uint8_t *)buf1, 20);
+
+  // Serial.println("Waiting for packet to complete..."); 
   // delay(10);
-  // rf95.send((uint8_t *)radiopacket, 20);
+  // rf95.waitPacketSent();
+  // // Now wait for a reply
+  // uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  // uint8_t len = sizeof(buf);
 
-  // Prepare a message to be sent
-  DateTime now = rtc.now();
-  char buf1[20];
-  sprintf(buf1, "%02d%02d%02d%02d%02d%02d",  now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second() );
-  //myString = buf1 + myString;
-  //Serial.println(myString);
-  rf95.send((uint8_t *)buf1, 20);
-
-  Serial.println("Waiting for packet to complete..."); 
-  delay(10);
-  rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  Serial.println("Waiting for reply...");
-  if (rf95.waitAvailableTimeout(1000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);    
-    }
-    else
-    {
-      Serial.println("Receive failed");
-    }
-  }
-  else
-  {
-    Serial.println("No reply, is there a listener around?");
-  }
-
-  delay(1000*5);
+  // Serial.println("Waiting for reply...");
+  // if (rf95.waitAvailableTimeout(1000))
+  // { 
+  //   // Should be a reply message for us now   
+  //   if (rf95.recv(buf, &len))
+  //  {
+  //     Serial.print("Got reply: ");
+  //     Serial.println((char*)buf);
+  //     Serial.print("RSSI: ");
+  //     Serial.println(rf95.lastRssi(), DEC);    
+  //   }
+  //   else
+  //   {
+  //     Serial.println("Receive failed");
+  //   }
+  // }
+  // else
+  // {
+  //   Serial.println("No reply, is there a listener around?");
+  // }
 
 }
 
